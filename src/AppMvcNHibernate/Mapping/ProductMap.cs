2427e8ef660fae1c353cfe1e1e3d3eb5bc95f11b
Misadri2 <mis.monterio@hotmail.com>
2022-06-03 14:45:28 -0300

    Criação de Mapping para produtos

---
+++ src/AppMvcNHibernate/Mapping/ProductMap.cs
@@ -0,0 +1,43 @@
+﻿using AppMvcNHibernate.Models;
+using NHibernate;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace AppMvcNHibernate.Mapping
+{
+    public class ProductMap : ClassMapping<Product>
+    {
+        public ProductMap()
+        {
+            Id(x => x.Id, x =>
+            {
+                x.Generator(Generators.Increment);
+                x.Type(NHibernateUtil.Int64);
+                x.Column("Id");
+            });
+
+            Property(b => b.Name, x =>
+            {
+                x.Length(520);
+                x.Type(NHibernateUtil.String);
+                x.NotNullable(true);
+            });
+
+            Property(b => b.Quantity, x =>
+            {
+                x.Type(NHibernateUtil.Int32);
+                x.NotNullable(true);
+            });
+
+            Property(b => b.Price, x =>
+            {
+                x.Type(NHibernateUtil.Double);
+                x.Scale(2);
+                x.Precision(15);
+                x.NotNullable(true);
+            });
+
+            Table("Products");
+        }
+    }
+}
